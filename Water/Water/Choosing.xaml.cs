f90d9effe6511886c9636f4094b73bfd440a7927
Anna <anuta.denisovna@gmail.com>
2020-01-24 19:44:30 +1000

    Shit happens

---
+++ Water/Water/Choosing.xaml.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Water
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Choosing : ContentPage 
+    {
+        Label header1;
+        Label header2;
+        public static int amount = 0;
+        public static object Selected;
+        public Choosing()
+        {
+            InitializeComponent();
+        }
+        public void Choose(object sender, EventArgs e)
+        {
+            amount = 0;
+            header1 = new Label
+            {
+                Text = "Choose drink",
+                FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label))
+            };
+
+            Picker picker = sender as Picker;
+
+            //picker.SelectedIndexChanged += Picker_SelectedIndexChanged;
+
+            Selected = picker.SelectedItem;
+
+            switch (Selected)
+            {
+                case "Water":
+                    image.Source = "water.jpg";
+                    break;
+                case "Juice":
+                    image.Source = "Juice.jpg";
+                    break;
+                case "Wine":
+                    image.Source = "wine.jpg";
+                    break;
+                case "Something stronger":
+                    image.Source = "Stronger.jpg";
+                    break;
+            }
+
+            Stepper stepper = new Stepper
+            {
+                Minimum = 0,
+                Maximum = 100,
+                Increment = 1,
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.CenterAndExpand
+            };
+            stepper.ValueChanged += OnStepperValueChanged;
+
+
+
+            header2 = new Label
+            {
+                Text = String.Format("{0}", stepper.Value),
+                FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
+                HorizontalOptions = LayoutOptions.Center
+            };
+
+            Button button_add = new Button
+            {
+                Text = "Add to order",
+                VerticalOptions = LayoutOptions.CenterAndExpand,
+                HorizontalOptions = LayoutOptions.Center,
+                BackgroundColor = Color.Pink
+            };
+
+            button_add.Clicked += Button_Clicked_1;
+
+
+            this.Content = new StackLayout { Children = { header1, picker, image, header2, stepper, button_add } };
+        }
+
+        private void OnStepperValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            header2.Text = String.Format("You chose: {0}", e.NewValue);
+            amount = Convert.ToInt32(e.NewValue);
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            if(amount > 0)
+            {
+                Chosen chosen = new Chosen();
+                Navigation.PushAsync(chosen);
+            }
+         }
+    }
+}
---
+++ Water/Water/Chosen.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+ 
+namespace Water
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Chosen : ContentPage
+    {
+        public struct Product
+        {
+            public object item;
+            public int count;
+            public Image image;
+        }
+        public ObservableCollection<Product> Basket = new ObservableCollection<Product>();
+        public Chosen()
+        {
+            InitializeComponent();
+            listView.ItemsSource = Basket;
+        }
+
+        public void AddToList()
+        {
+            Product product = new Product();
+            Label header = new Label
+            {
+                Text = "Your order",
+                FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
+                HorizontalOptions = LayoutOptions.Center
+            };
+            product.item = Choosing.Selected;
+            product.count = Choosing.amount;
+            Image img = new Image();
+
+            switch (product.item)
+            {
+                case "Water":
+                    img.Source = "water.jpg";
+                    product.image = img;
+                    break;
+                case "Juice":
+                    img.Source = "Juice.jpg";
+                    product.image = img;
+                    break;
+                case "Wine":
+                    img.Source = "wine.jpg";
+                    product.image = img;
+                    break;
+                case "Something stronger":
+                    img.Source = "Stronger.jpg";
+                    product.image = img;
+                    break;
+            }
+            Basket.Add(product);
+        }
+        private void Button_Clicked_Back(object sender, EventArgs e)
+        {
+            if (Basket.Count == 0)
+            {
+                DisplayAlert("Ooooops", "You chose nothing, dear", "Choose");
+            }
+            else
+            {
+                DisplayAlert("Thank you!", "We`ve got your order, dear.", "Bye-Bye");
+            }
+            Basket.Clear();
+            Choosing.amount = 0;
+            MainPage mainPage = new MainPage();
+            Navigation.PushAsync(mainPage);
+        }
+        private void Button_Clicked_Add(object sender, EventArgs e)
+        {
+            Choosing choosing = new Choosing();
+            Navigation.PushAsync(choosing);
+        }
+    }
+}
